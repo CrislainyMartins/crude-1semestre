c4d714772a322112f940e1cdc60fa43a121f9aa7
CrislainyMartins <crislainy6339@gmail.com>
2024-06-20 08:26:23 -0300

    deu certo
    dessa vez funcionou. o erro era apenas o caminho para abrir o arquivo.

--- excluir.php
+++ excluir.php
@@ -1,15 +1,50 @@
-<?php 
-include ("conexao.php");
-$id= $_POST['id'];
-$nome= $_POST['nome'];
 
+<!doctype html>
+<html lang="en">
 
-$sql =" DELETE FROM usuario WHERE id=$id";
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Sucesso</title>
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  </head>
 
-if ($sql=mysqli_query($conexao, $sql)){
-    echo"cadastro realizado com sucesso";
+<body>
+  <div class="container">
+      <div class="coluna">
+
+<?php
+
+include "conexao.php";
+
+$id = $_POST['id'];
+$nome = $_POST['nome'];
+
+$sql = "DELETE FROM usuario WHERE id = $id";
+
+if($sql = mysqli_query($conexao,$sql)){
+
+    echo"$nome Excluido com sucesso!";
+
 }
 else{
-    echo"erro ao se cadastrar com o banco".mysqli_connect_error($conexao);
+
+    echo"error ao Excluir usuairo";
+    
 }
+
+
+
 ?>
+
+<button class="btn btn-success"><a href="pesquisa.php" style="text-decoration: none; color:white;">Voltar</a></button>
+
+      </div>
+  </div>
+
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+  </body>
+
+</html>
